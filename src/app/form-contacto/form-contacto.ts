5e8d8d3494a644208d50031fadf6e4b3719864dc
LauraMolano <laurapaola.molano172@comunidadunir.net>
2025-11-10 20:58:11 -0500

    Creación y activación de pop-up Confirmación de envío de formulario de contáctanos

--- src/app/form-contacto/form-contacto.ts
+++ src/app/form-contacto/form-contacto.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-contacto',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule], 
   templateUrl: './form-contacto.html',
   styleUrl: './form-contacto.css'
 })
 export class FormContacto implements OnInit{
   title = 'Contáctanos';
   formBuilder = inject(FormBuilder);
+  
+  isModalVisible: boolean = false; 
+
   formComunicaciones = this.formBuilder.group({
-    name: new FormControl('', [Validators.required, Validators.minLength(10)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
-    messaje:new FormControl('', [Validators.required, Validators.minLength(50)]), 
+    name: new FormControl('', []), 
+    email: new FormControl('', []),
+    phone: new FormControl('', []),
+    messaje: new FormControl('', []), 
   });
+
   ngOnInit(): void {
   }
+  
   onSubmit() {
-    console.log(this.formComunicaciones.value);
+    console.log('Intento de envío. Mostrando popup.');
+    this.showModal();
+    this.formComunicaciones.reset();
+  }
+  showModal() {
+    this.isModalVisible = true;
+  }
+  hideModal() {
+    this.isModalVisible = false;
   }
 };
- 
